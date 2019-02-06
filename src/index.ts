19b7c46acf5bd737bbe6044650de52cf9ffa2bd8
Dominic Ginger <5877689+DominicGinger@users.noreply.github.com>
2019-02-06 23:17:39 +0100

    Add support for more config options

--- src/index.ts
+++ src/index.ts
@@ -3,42 +3,83 @@ import jsYaml from 'js-yaml'
 
 const SCHEMA_VERSION = 'http://json-schema.org/draft-07/schema#'
 
-type OptionalFields = {
+type Fields = {
   [x: string]: [
     string
   ]
 }
+
+enum Required {
+  ALL = 'all',
+  NONE = 'none',
+  RESPECT = 'respect'
+}
+
+type Config = {
+  optionalFields?: Fields
+  requiredFields?: Fields
+  required: Required
+}
 let definitions: object = {}
-let optionalFields: OptionalFields = {}
+let fields: Fields = {}
+let required: Required = Required.RESPECT
 const schema = { definitions: {}, paths: {} }
 
-function convertDefinition (oDefinition: any) {
+function getRequiredObj (definition) {
+  switch (required) {
+    case Required.RESPECT:
+      return definition.required || []
+    case Required.ALL:
+      return Object.keys(definition.properties).filter(x => (fields[definition['$name']] || []).indexOf(x) === - 1)
+    case Required.NONE:
+      return fields[definition['$name']] || []
+  }
+}
+
+function allOf (definition) {
+  const refDefinition = replaceRefs(definition.allOf[0])
+  definition.properties = { ...refDefinition.properties, ...definition.allOf[1].properties }
+
+  definition.required = getRequiredObj(definition)
+  definition.additionalProperties = false
+  delete definition.allOf
+
+  return definition
+}
+
+function typeObject (definition) {
+  definition.properties = replaceRefs(definition.properties)
+
+  definition.required = getRequiredObj(definition)
+  definition.additionalProperties = false
+
+  return definition
+}
+
+function typeArray (definition) {
+  definition.items = replaceRefs(definition.items)
+  return definition
+}
+
+function convertDefinition (oDefinition) {
   let definition = JSON.parse(JSON.stringify(oDefinition || {}))
   if (definition.allOf) {
-    const refDefinition = replaceRefs(definition.allOf[0])
-    definition.properties = { ...refDefinition.properties, ...definition.allOf[1].properties }
-
-    definition.required = Object.keys(definition.properties).filter(x => (optionalFields[definition['$name']] || []).indexOf(x) === - 1)
-    definition.additionalProperties = false
-    delete definition.allOf
+    return allOf(definition)
   }
   if (definition['$ref']) {
     return replaceRefs(definition)
   }
   if (definition.type === 'object') {
-    definition.properties = replaceRefs(definition.properties)
-
-    definition.required = Object.keys(definition.properties).filter(x => (optionalFields[definition['$name']] || []).indexOf(x) === - 1)
-    definition.additionalProperties = false
+    return typeObject(definition)
   }
   if (definition.type === 'array') {
-    definition.items = replaceRefs(definition.items)
+    return typeArray(definition)
   }
 
   return definition
 }
 
-function replaceRefs (oDefinition: any) {
+function replaceRefs (oDefinition) {
   let definition = JSON.parse(JSON.stringify(oDefinition || {}))
   Object.keys(definition).forEach(key => {
     if (key === '$ref') {
@@ -51,14 +92,25 @@ function replaceRefs (oDefinition: any) {
   return definition
 }
 
-export default function convert (filePath: string, optionalFields: OptionalFields): any {
+export default function convert (filePath: string, config: Config) {
   if (!fs.existsSync(filePath)) {
     throw Error(`Could not find swagger file at: ${filePath}`)
   }
   const file = fs.readFileSync(filePath).toString('utf-8')
   const data = jsYaml.safeLoad(file)
   definitions = data.definitions
-  optionalFields = optionalFields
+  required = config.required
+  switch (required) {
+    case Required.RESPECT:
+      fields = {}
+      break
+    case Required.ALL:
+      fields = config.optionalFields || {}
+      break
+    case Required.NONE:
+      fields = config.requiredFields || {}
+      break
+  }
 
   Object.keys(definitions).forEach(key => {
     const definition = definitions[key]
@@ -82,9 +134,15 @@ export default function convert (filePath: string, optionalFields: OptionalField
     })
   })
 
-  // console.log(data)
-  console.log(JSON.stringify(schema.paths))
   return schema
 }
 
-convert(process.argv[3], {})
+const config: Config = {
+  required: Required.ALL,
+  optionalFields: {
+    Pet: [ 'id' ]
+  }
+}
+
+convert(process.argv[3], config)
+console.log(JSON.stringify(schema.paths))
